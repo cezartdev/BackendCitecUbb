3d97767fe7d51b5a5d6ae1a43c070224a9b99e9b
Joaquín Ávila <joaquinaviladelgado@gmail.com>
2024-09-26 17:28:25 -0300

    arreglo error giros, se deja comentado

--- backend_citec_ubb/src/models/BussinessLine.model.ts
+++ backend_citec_ubb/src/models/BussinessLine.model.ts
@@ -1,35 +1,34 @@
-import db from "../config/db"
+// import db from "../config/db"
 
-class BussinessLine {
-    //Modelo SQL de la clase
-    static async initTable(): Promise<void> {
-        const createTableQuery = `
-            CREATE TABLE IF NOT EXISTS giros (
-                codigo INT PRIMARY KEY,
-                nombre VARCHAR(200) NOT NULL,
-                nombre_categoria VARCHAR(200) NOT NULL,
-                FOREIGN KEY (nombre_categoria) REFERENCES categorias(nombre).
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            );
-        `;
-        const insertDataQuery = `
-            INSERT INTO giros (nombre) VALUES
-            ('construccion'),
-            ('agricultura'),
-            ON DUPLICATE KEY UPDATE nombre = VALUES(nombre);
-        `;
+// class BussinessLine {
+//     //Modelo SQL de la clase
+//     static async initTable(): Promise<void> {
+//         const createTableQuery = `
+//             CREATE TABLE IF NOT EXISTS giros (
+//                 codigo INT PRIMARY KEY,
+//                 nombre VARCHAR(200) NOT NULL,
+//                 nombre_categoria VARCHAR(200) NOT NULL,,
+//                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//             );
+//         `;
+//         const insertDataQuery = `
+//             INSERT INTO giros (nombre) VALUES
+//             ('construccion'),
+//             ('agricultura'),
+//             ON DUPLICATE KEY UPDATE nombre = VALUES(nombre);
+//         `;
 
-        try {
-            // Crear la tabla si no existe
-            await db.query(createTableQuery);
-            // Insertar valores por defecto si es necesario
-            await db.query(insertDataQuery);
-        } catch (err) {
-            console.error('Error al inicializar la tabla tipo:', err);
-            throw err;
-        }
-    }
+//         try {
+//             // Crear la tabla si no existe
+//             await db.query(createTableQuery);
+//             // Insertar valores por defecto si es necesario
+//             await db.query(insertDataQuery);
+//         } catch (err) {
+//             console.error('Error al inicializar la tabla tipo:', err);
+//             throw err;
+//         }
+//     }
 
-}
+// }
 
-export default BussinessLine;
+// export default BussinessLine;
